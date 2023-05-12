c19801c2feb962c0618f8d7a3ffeb8681873296b
Sergio Ramirez <sergiodan@Sergios-Mac-mini.local>
2023-05-12 15:24:41 -0700

    Add camera libraries. Create Camera preview composable. Add context extension functions for camera provider

---
+++ app/src/main/java/com/accretiond/giftcard/composables/CameraPreview.kt
@@ -0,0 +1,60 @@
+package com.accretiond.giftcard.composables
+
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.view.PreviewView
+import androidx.camera.view.PreviewView.ScaleType
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.coroutines.launch
+
+@Composable
+fun CameraPreview(
+    modifier: Modifier,
+    scaleType: ScaleType = ScaleType.FILL_CENTER,
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(
+        modifier = modifier,
+        factory = {context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+
+            val previewUseCase = Preview
+                .Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            coroutineScope.launch {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        previewUseCase
+                    )
+                } catch(exception: Exception) {
+                    Log.e("CameraPreview", "Lifecycle - Use case binding failed", exception)
+                }
+            }
+
+            previewView
+        }
+    )
+}
